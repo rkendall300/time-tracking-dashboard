69dcdd4b5dabf14890ccc4eceb41eef847bc6fe4
Ryan Kendall <ryekendallion@gmail.com>
2024-02-23 20:12:29 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,70 +1,56 @@
-# Getting Started with Create React App
+# Frontend Mentor - Time tracking dashboard solution
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
 
-## Available Scripts
+## Table of contents
 
-In the project directory, you can run:
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
 
-### `npm start`
+## Overview
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+### The challenge
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Users should be able to:
 
-### `npm test`
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### Screenshot
 
-### `npm run build`
+## Desktop Design
+![](./desktop-design.PNG)
+![](./desktop-active.PNG)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Mobile Design
+![](./mobile-design.PNG)
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+### Links
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+- Live Site URL: [Time Tracking Dashboard](https://rkendall300.github.io/time-tracking-dashboard/)
 
-### `npm run eject`
+## My process
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+### Built with
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+### What I learned
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+This was my first project fully utilizing the json data provided by Frontend Mentor. I honestly didn't even notice that there were different displays for daily, weekly, and monthly selections, I just figured it was there for the hover state as shown in the active-states picture provided. After reading the README I saw that it was supposed to swap the data in and out, and decided to try and learn how to use json files. I think it turned out pretty well! Very happy with the result.
 
-## Learn More
+## Author
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- Frontend Mentor - [@rkendall300](https://www.frontendmentor.io/profile/rkendall300)
--- src/App.js
+++ src/App.js
@@ -1,24 +1,129 @@
-import logo from './logo.svg';
 import './App.css';
+import jeremyLogo from './images/image-jeremy.png';
+import ellipsis from './images/icon-ellipsis.svg';
+import data from './data.json';
+import { useState } from 'react';
 
 function App() {
+
+  const [timeFrame, setTimeFrame] = useState("weekly");
+  const [previousFrame, setPreviousFrame] = useState("Last Week");
+
+  const setDisplay = (e) => {
+    switch (e.target.id) {
+      case "daily":
+        setTimeFrame("daily");
+        setPreviousFrame("Yesterday");
+        break;
+      case "weekly":
+        setTimeFrame("weekly");
+        setPreviousFrame("Last Week");
+        break;
+      case "monthly":
+        setTimeFrame("monthly");
+        setPreviousFrame("Last Month");
+        break;
+      default:
+        // Do nothing
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div id="backdrop">
+        <div id="container">
+          <div id="intro">
+            <div id="top-intro">
+              <img src={jeremyLogo} alt="avatar" id="avatar"></img>
+              <h1 id="intro-description">Report for <span className="name">Jeremy Robson</span></h1>
+            </div>
+            <div id="bottom-intro">
+              <p className={(timeFrame === "daily") ? "intro-time-selected" : "intro-time"} id="daily" onClick={setDisplay}>Daily</p>
+              <p className={(timeFrame === "weekly") ? "intro-time-selected" : "intro-time"} id="weekly" onClick={setDisplay}>Weekly</p>
+              <p className={(timeFrame === "monthly") ? "intro-time-selected" : "intro-time"} id="monthly" onClick={setDisplay}>Monthly</p>
+            </div>
+          </div>
+          <div className="card" id="work-card">
+            <div className="card-bg" id="work-bg"></div>
+            <div className="card-backdrop">
+              <div className="top-line">
+                <p className="title">Work</p>
+                <img src={ellipsis} className="ellipse" alt="dots"></img>
+              </div>
+              <div className="card-info">
+                <p className="hours">{data[0].timeframes[timeFrame].current}hrs</p>
+                <p className="previous">{previousFrame} - {data[0].timeframes[timeFrame].previous}hrs</p>
+              </div>
+            </div>
+          </div>
+          <div className="card" id="play-card">
+            <div className="card-bg" id="play-bg"></div>
+            <div className="card-backdrop">
+              <div className="top-line">
+                <p className="title">Play</p>
+                <img src={ellipsis} className="ellipse" alt="dots"></img>
+              </div>
+              <div className="card-info">
+                <p className="hours">{data[1].timeframes[timeFrame].current}hrs</p>
+                <p className="previous">{previousFrame} - {data[1].timeframes[timeFrame].previous}hrs</p>
+              </div>
+            </div>
+          </div>
+          <div className="card" id="study-card">
+            <div className="card-bg" id="study-bg"></div>
+            <div className="card-backdrop">
+              <div className="top-line">
+                <p className="title">Study</p>
+                <img src={ellipsis} className="ellipse" alt="dots"></img>
+              </div>
+              <div className="card-info">
+                <p className="hours">{data[2].timeframes[timeFrame].current}hrs</p>
+                <p className="previous">{previousFrame} - {data[2].timeframes[timeFrame].previous}hrs</p>
+              </div>
+            </div>
+          </div>
+          <div className="card" id="exercise-card">
+            <div className="card-bg" id="exercise-bg"></div>
+            <div className="card-backdrop">
+              <div className="top-line">
+                <p className="title">Exercise</p>
+                <img src={ellipsis} className="ellipse" alt="dots"></img>
+              </div>
+              <div className="card-info">
+                <p className="hours">{data[3].timeframes[timeFrame].current}hrs</p>
+                <p className="previous">{previousFrame} - {data[3].timeframes[timeFrame].previous}hrs</p>
+              </div>
+            </div>
+          </div>
+          <div className="card" id="social-card">
+            <div className="card-bg" id="social-bg"></div>
+            <div className="card-backdrop">
+              <div className="top-line">
+                <p className="title">Social</p>
+                <img src={ellipsis} className="ellipse" alt="dots"></img>
+              </div>
+              <div className="card-info">
+                <p className="hours">{data[4].timeframes[timeFrame].current}hrs</p>
+                <p className="previous">{previousFrame} - {data[4].timeframes[timeFrame].previous}hrs</p>
+              </div>
+            </div>
+          </div>
+          <div className="card" id="self-care-card">
+            <div className="card-bg" id="self-care-bg"></div>
+            <div className="card-backdrop">
+              <div className="top-line">
+                <p className="title">Self Care</p>
+                <img src={ellipsis} className="ellipse" alt="dots"></img>
+              </div>
+              <div className="card-info">
+                <p className="hours">{data[5].timeframes[timeFrame].current}hrs</p>
+                <p className="previous">{previousFrame} - {data[5].timeframes[timeFrame].previous}hrs</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
